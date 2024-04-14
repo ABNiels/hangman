4bbdbc0c71fd4b0eef604eafe3252d815316aaac
Nielsen <andersbrnielsen@gmail.com>
2024-04-14 15:56:48 -0400

    Different syntax for the file read

--- src/word_gen.rs
+++ src/word_gen.rs
@@ -1,21 +1,23 @@
 pub mod word_gen {
     use std::fs::File;
-    use std::io::{BufRead, BufReader, };
+    use std::io::{BufRead, BufReader};
     use rand::Rng;
 
     pub fn gen_word() -> String {
-        // Perform some operations here
-        println!("Generating word...");
-        let f = File::open("words.txt").expect("Unable to open file\n");
+        // Apparently Rust closes the file when it goes out of scope. This is really just to test the syntax of this
+        let line_count: usize = 
+        {
+            let f = File::open("words.txt").expect("Unable to open file\n");
+            let reader: BufReader<File> = BufReader::new(f);
+            reader.lines().count()
+        };
 
-        let reader: BufReader<File> = BufReader::new(f);
-        let line_count: usize = reader.lines().count();
         let word_index: usize = rand::thread_rng().gen_range(0..line_count);
 
-        // reopen file because I can't for the life of me figure out how to seek the start
-        let f = File::open("words.txt").expect("Unable to open file\n");
-        let reader = BufReader::new(f);
-
-        reader.lines().nth(word_index).unwrap().unwrap()
+        {
+            let f = File::open("words.txt").expect("Unable to open file\n");
+            let reader = BufReader::new(f);
+            reader.lines().nth(word_index).unwrap().unwrap()
+        }
     }
 }
